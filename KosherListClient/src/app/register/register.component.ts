bfb5d1b8592f4e028dada3f2fd0e483a54b65fdb
elizabethbringer <elizabethbringer@gmail.com>
2020-12-28 23:09:25 +0200

    28-12

--- KosherListClient/src/app/register/register.component.ts
+++ KosherListClient/src/app/register/register.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Worker } from '../classes/Worker';
 import { GetServerService } from '../get-server.service';
+import { WorkerService } from '../worker.service';
 
 @Component({
   selector: 'app-register',
@@ -9,41 +11,51 @@ import { GetServerService } from '../get-server.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  workers:Worker[];
+  form = this.fb.group({
+    nameWorker: this.fb.control('', [Validators.required]),
+    addressWorker: this.fb.control('', [Validators.required]),
+    telWorker: this.fb.control('', [Validators.required]),
+    dateStart: this.fb.control('', [Validators.required]),
+    email: this.fb.control('', [Validators.required]),
+    mobility: this.fb.control('', [Validators.required]),
+    
+  });
   flagMail: boolean = false;
-  constructor(private service: GetServerService, private http: HttpClient) { }
+  constructor(private service: WorkerService, private http: HttpClient,private fb:FormBuilder) { }
 
   ngOnInit() {
   }
   flag: any = false;
-  worker: Worker = new Worker();
+  // worker: Worker = new Worker();
   
+// if (this.worker.email.indexOf("@") > -1)
+      //  this.flagMail = true;
 
   flagUser: boolean;
   register() {
-     if (this.worker.email.indexOf("@") > -1)
+    const worker = <Worker> this.form.value;
+    this.service.createWorker(worker).subscribe(x=>{
+     
+    })};
+     
 
-       this.flagMail = true;
-
-    this.service.getWorker()
-      .subscribe(t => {
-       let workertmp:Worker = t.find(y => y.nameWorker == this.worker.nameWorker);
-        if (workertmp === undefined && this.flagMail == true) {
-          this.service.postWorker(this.worker)
-          .subscribe(r => {
-            if (r) { this.flag = true; }
-            else { this.flag = false }
-            alert(r);
-            console.log(r);
-          });
-        }
-        else {
-          this.worker=workertmp;
-          this.flagUser = true;
-        }
-      });
+    // this.service.getWorker()
+    //   .subscribe(t => {
+    //    let workertmp:Worker = t.find(y => y.nameWorker == this.worker.nameWorker);
+    //     if (workertmp === undefined && this.flagMail == true) {
+    //       this.service.postWorker(this.worker)
+    //       .subscribe(r => {
+    //         if (r) { this.flag = true; }
+    //         else { this.flag = false }
+    //         alert(r);
+    //         console.log(r);
+    //       });
+        // }
+        // else {
+        //   this.worker=workertmp;
+        //   this.flagUser = true;
+        // }
+      // });
 
   }
-
-
-}
